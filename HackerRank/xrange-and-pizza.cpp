54ec312434722310c1040e1558013806c18c30c3
AbdelrahmanRamadan <abdelrahman0xff@gmail.com>
2017-12-31 16:32:28 +0200

    add solution

--- HackerRank/xrange-and-pizza.cpp
+++ HackerRank/xrange-and-pizza.cpp
@@ -10,10 +10,15 @@
 	@author: Abdelrahman Ramadan
 	@email: abdelrahman0xff@gmail.com
 	@repo: https://github.com/AbdelrahmanRamadan/competitive-programming
-	@problem:
-	@link:
-	@judge:
-	@idea:
+	@problem: Xrange's Pancakes
+	@link: https://www.hackerrank.com/challenges/xrange-and-pizza/problem
+	@judge: HackerRank
+	@idea: fix the position of the first vertex (variable rot) starting with zero
+		* when performing rotation add the rotation amount to rot: rot = (rot + k) % n
+		* when performing reflection add the amount that our fixed vertex moved by after
+			the reflection: rot = (rot + (k - rot * 2)) % n
+		this amount comes from the fact that the vertext travels double the distance between it and the reflection axis
+
 */
 
 #include <bits/stdc++.h>
@@ -23,18 +28,20 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 	int rot = 0;
-	bool flip = 1;
+    int is_ref = 0;
 	while (m--) {
 		int type, k;
 		cin >> type >> k;
-		rot = (rot + k) % n;
-		if (type == 2) {
-			flip ^= 1;
+		if (type == 1) {
+			rot = (rot + k) % n;
+		} else {
+            rot = (rot + (k - rot * 2) + n) % n;
+			is_ref ^= 1;
 		}
 	}
-	if (flip)
+	if (is_ref)
 		cout << 2 << ' ' << rot << endl;
 	else
-		cout << 1 << ' ' << n - rot << endl;
+		cout << 1 << ' ' << (n - rot) % n << endl;
 	return 0;
 }
